f68565ac88ca1ab6beb59ae0ef110068888a6349
stepan TeSt <stepan.prosin@mail.ru>
2023-10-29 16:00:28 +0300

    reformat complite

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,8 @@
 public class CreditPaymentService {
-    public  int calculate(double credit, double rate,int years) {
-
-        int month =years * 12;
-        double monthrate = rate/12/100;
-
-        int monthpay =(int)(credit*(monthrate*Math.pow((1+monthrate),month))/(-1+Math.pow((1+monthrate),month)));
-
-        return  monthpay;
+    public int calculate(double credit, double rate, int years) {
+        int month = years * 12;
+        double monthrate = rate / 12 / 100;
+        int monthpay = (int) (credit * (monthrate * Math.pow((1 + monthrate), month)) / (-1 + Math.pow((1 + monthrate), month)));
+        return monthpay;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         double credit = 1000000;
         double rate = 9.99;
-
-
-        int month = service.calculate(credit,rate,1);
+        int month = service.calculate(credit, rate, 1);
         System.out.println(month);
-        month = service.calculate(credit,rate,2);
+        month = service.calculate(credit, rate, 2);
         System.out.println(month);
-        month = service.calculate(credit,rate,3);
+        month = service.calculate(credit, rate, 3);
         System.out.println(month);
     }
 }
