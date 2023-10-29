c36217d33e15d051f4482324a87d183bdac10825
stepan TeSt <stepan.prosin@mail.ru>
2023-10-29 15:28:52 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        double credit = 1000000;
+        double rate = 9.99;
+
+
+        int month = service.calculate(credit,rate,1);
+        System.out.println(month);
+        month = service.calculate(credit,rate,2);
+        System.out.println(month);
+        month = service.calculate(credit,rate,3);
+        System.out.println(month);
+    }
+}
